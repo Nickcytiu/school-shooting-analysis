62d3433233c9c18bd1d2ec2d2e204fb49442cbe9
Nick Tiu <nick.tiu@ryerson.ca>
2021-10-19 22:09:05 -0400

    update

--- wiki_data_pull.py
+++ wiki_data_pull.py
@@ -25,7 +25,11 @@ regex_pattern = '([A-Za-z, 0-9]+)\\n'
 regex_loc = '([A-Za-z, .]*)\\n'
 regex_num = '(^\d?\d?\d?\d)'
 regex_des = '(?<![\\\[])[^\\\[\]]+'
-regex_school = '(((([A-Z]\w+ )?([A-Z]\w+ )?([A-Z])(\w+-\w+)?(\w*\.\w*\. )?([^(a|A)t ]\w* )?([A-Z][\w+ )?([^at ]\w+\,? )?(\w\.)?(\w+\. )?)|([A-Z]\w+ )([A-Z]\w+ )?([A-Z]\w+ )?)(((e|E)lementary |(m|M)iddle |(h|H)igh )?((s|S)chool)|(c|C)ollege|(u|U)niversity|(c|C)ollegiate|(i|I)nstitute (of )? ([A-Z]\w+ )|(s|S)tadium|(C)ampus|(a|A)cademy (of )?([A-Z]\w+ )?([A-Z]\w+ ?)?([A-Z]\w+)?)|(school bus)|(([A-Z]\w+ )?((u|U)niversity |(c|C)ollege |(a|A)cademy |(i|I)nstitute )((of )(the )?(([A-Z])(\w+ ?))([A-Z]\w+ )?([A-Z]\w+)?))|(s|S)tadium|(\w+ )(Elementary)|(UCLA))'
+regex_school1 = '(((([A-Z]\w+ )?([A-Z]\w+ )?([A-Z])(\w+-\w+)?(\w*\.\w*\. )?([^(a|A)t ]\w* )?([A-Z][\w+ )?([^at ]\w+\,? )?(\w\.)?(\w+\. )?)|([A-Z]\w+ )([A-Z]\w+ )?([A-Z]\w+ )?)(((e|E)lementary |(m|M)iddle |(h|H)igh )?((s|S)chool)|(c|C)ollege|(u|U)niversity|(c|C)ollegiate|(i|I)nstitute (of )? ([A-Z]\w+ )|(s|S)tadium|(C)ampus|(a|A)cademy (of )?([A-Z]\w+ )?([A-Z]\w+ ?)?([A-Z]\w+)?)|(school bus)|(([A-Z]\w+ )?((u|U)niversity |(c|C)ollege |(a|A)cademy |(i|I)nstitute )((of )(the )?(([A-Z])(\w+ ?))([A-Z]\w+ )?([A-Z]\w+)?))|(s|S)tadium|(\w+ )(Elementary)|(UCLA))'
+regex_school2 = '(?<=At |An )[A-Za-z]\w*\.? ?\w+\.? ?\w+\.? ?\w+\.?'
+regex_insti = '(\w+ (?=(s|S)chool)\w+|(c|C)ollege|(u|U)niversity|(c|C)ollegiate|(i|I)nstitute (of )? ([A-Z]\w+ )|(C)ampus|(a|A)cademy)'
+regex_state = '(?<=\, )[A-Z]\w*\.? ?\w+\.? ?'
+regex_city = '\w+\.? ?\w+? ?\w+?(?=\,)'
 
 # Function to extract info
 def _extract_date(text):
@@ -52,8 +56,25 @@ def _extract_description(text):
     return description
 
 def _extract_school(text):
-    school = re.findall(regex_school, text)
+    school = re.findall(regex_school1, text)
     return school
+
+def _extract_school2(text):
+    school = re.findall(regex_school2, text)
+    return school
+
+def _extract_city(text):
+    city = re.findall(regex_city, text)
+    return city
+    
+def _extract_state(text):
+    state = re.findall(regex_state, text)
+    return state
+
+def _extract_insti(text):
+    insti = re.findall(regex_insti, text)
+    return insti
+
 
 # Function to create dict
 def parse_table_rows(table_rows):
@@ -72,6 +93,18 @@ def parse_table_rows(table_rows):
                 
                 location_raw = data_cols[1].text
                 location = _extract_location(location_raw)
+                if location == "Wisconsin":
+                    city = pd.NA
+                    state = "Wisconsin"
+                elif location == "Washington D.C.":
+                    city = "Washington"
+                    state = "D.C."
+                elif location == "Detroit Michigan":
+                    city = "Detroit"
+                    state = "Michigan"
+                else:
+                    city = _extract_city(location)[0]
+                    state = _extract_state(location)[0]
                 
                 school = _extract_school(data_cols[4].text)
                 if school == []:
@@ -79,19 +112,26 @@ def parse_table_rows(table_rows):
                     school = pd.NA
                 else:
                     school = school[0][0]
-#                     print(school)
-
+                    
+                    if school.startswith('An ') or school.startswith('At '):
+                        school = _extract_school2(school)
+                        school = school[0]
+                    insti = _extract_insti(school)
+                    if insti == []:
+                        insti = pd.NA
+                    else:
+                        insti = insti[0][0]
                 death_raw = data_cols[2].text
                 injury_raw = data_cols[3].text
                 description_raw = data_cols[4].text
             
-                if "n 1" in death_raw and "n 1" in injury_raw:
+                if "n " in death_raw and "n " in injury_raw:
                     DIS = True
                     IIS = True
-                elif "n 1" in death_raw:
+                elif "n " in death_raw:
                     DIS = True
                     IIS = False
-                elif "n 1" in injury_raw:
+                elif "n " in injury_raw:
                     IIS = True
                     DIS = False
                 else:
@@ -103,28 +143,31 @@ def parse_table_rows(table_rows):
                 else:
                     injury = _extract_injury(injury_raw)
                 description = _extract_description(description_raw)
-
+                
                 row_data_raw = {'Date': date_raw, 
                                 'Location': location_raw,
-                                'Death': death_raw,
                                 'Injury': injury_raw,
+                                'Death': death_raw,
                                 'Description': description_raw}
                 table_data_raw[ix] = row_data_raw
                 
                 row_data = {'Date': date, 
-                            'Location': location, 
-                            'School': school,
-                            'Death': death,
-                            'Death_Including_Suspect': DIS,
+                            'Location': location,
+                            'City': city,
+                            'State': state,
+                            'School_Name': school,
+                            'Institution_Type': insti,
                             'Injury': injury, 
                             'Injury_Including_Suspect': IIS,
-                            'Description': description}
+                            'Death': death,
+                            'Death_Including_Suspect': DIS,
+                            'Description': description
+                            }
                 table_data[ix] = row_data
-                
+
         return table_data, table_data_raw
     except:
         print("No data")
-
 
 response_19 = requests.get(url_1900s)
 response_20 = requests.get(url_2000s)
@@ -143,6 +186,8 @@ df_20_raw = pd.DataFrame.from_dict(t_s_20_raw, orient = 'index')
 
 df_19 = pd.DataFrame.from_dict(t_s_19, orient = 'index')
 df_20 = pd.DataFrame.from_dict(t_s_20, orient = 'index')
+
+print(df_20["Institution_Type"])
 
 df_20_raw.to_gbq(destination_table="wiki.wiki_20_raw", project_id=gcp_project, if_exists="replace")
 df_19_raw.to_gbq(destination_table="wiki.wiki_19_raw", project_id=gcp_project, if_exists="replace")
